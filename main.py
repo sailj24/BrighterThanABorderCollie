47703db027b7d0be4ec7db339fce993ef2e9c08d
Carlos Gomez <crgomez@udallas.edu>
2021-03-13 16:31:16 -0600

    Changes controller function for default page

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+import os
+from flask import jsonify, request, abort, json
 from flask import Flask, render_template
+import requests
+import pandas
+import ast
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+# Google Sheets API Setup
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json",["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/spreadsheets","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"])
+client = gspread.authorize(credential)
+gsheet = client.open("BS_Database").sheet1
+
+
+#Controller
+@app.route('/', methods=["GET"])
+def home():
+    data = json.dumps(gsheet.get_all_records())
+    lists = ast.literal_eval(data)
+    return render_template('home.html', result=lists)
+
+@app.route("/upload")
+def upload():
+    return render_template("upload.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
