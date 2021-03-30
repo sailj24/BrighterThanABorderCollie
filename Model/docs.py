5f6dc5e88540e171eec17570c92918f4513b586e
Carlos Gomez <crgomez@udallas.edu>
2021-03-30 16:57:16 -0500

    Added upload page with API with refresh fix

--- Model/docs.py
+++ Model/docs.py
@@ -1,20 +1,23 @@
 class doc:
-    id = 0
+    id = ''
     name = ''
     doc_URL = ''
     professor = ''
     gradeID = 0
     subjectID = 0
     create_user = ''
+    extension = ''
 
 
-    def __init__(self, id, name, doc_URL, gradeID, subjectID, user):
+    def __init__(self, id, name, doc_URL, gradeID, subjectID, user, extension, professor):
         self.id = id
         self.name = name
         self.doc_URL = doc_URL
         self.gradeID = gradeID
         self.subjectID = subjectID
         self.create_user = user
+        self.extension = extension
+        self.professor = professor
     
     def printName(self):
         print(self.name)
